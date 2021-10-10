f97424dbe0a1b10899e0d459ec11ecc1acd273ad
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-10-10 23:46:49 +0600

    Update ItemsController.cs

--- PlayCatelogServiceApp/Controllers/ItemsController.cs
+++ PlayCatelogServiceApp/Controllers/ItemsController.cs
@@ -40,5 +40,25 @@ namespace PlayCatelogServiceApp.Controllers
             return CreatedAtAction(nameof(GetItemById), new { id = item.Id }, item);
         }
 
+        [HttpPut("{id}")]
+        public ActionResult<ItemDto> Put(Guid id, UpdateItemDto updateItemDto)
+        {
+            var item = items.Where(x => x.Id == id).SingleOrDefault();
+
+            var updateItem = item with
+            {
+                Name = updateItemDto.Name,
+                Description = updateItemDto.Description,
+                Price = updateItemDto.Price,
+            };
+
+            var index = items.FindIndex(x => x.Id == id);
+            items[index] = updateItem;
+
+
+            return CreatedAtAction(nameof(GetItemById), new { id = item.Id }, item);
+        }
+
+
     }
 }
