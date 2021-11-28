57cb6cf7155618412f62dfd8735f25eafa1465ba
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-11-28 20:11:28 +0600

    Update Extensions.cs

--- PlayCatelogServiceApp/Helpers/Extensions.cs
+++ PlayCatelogServiceApp/Helpers/Extensions.cs
@@ -1,12 +1,39 @@
 ﻿using PlayCatelogServiceApp.Entities;
+using System;
 
 namespace PlayCatelogServiceApp.Helpers
 {
     public static class Extensions
     {
-        public static ItemDto asDto(this Item item)
+        public static ItemDto AsDto(this Item item)
         {
             return new ItemDto(item.Id, item.Name, item.Description, item.Price, item.CreatedDate);
         }
+
+        public static Item AsCreateModel(this CreateItemDto item)
+        {
+            return new Item()
+            {
+                Name = item.Name,
+                Description = item.Description,
+                Price = item.Price,
+                CreatedDate = DateTime.UtcNow
+            };
+        }
+
+        public static Item AsUpdateModel(this UpdateItemDto item, Guid id)
+        {
+            return new Item()
+            {
+                Id = id,
+                Name = item.Name,
+                Description = item.Description,
+                Price = item.Price,
+                CreatedDate = DateTime.UtcNow
+            };
+        }
+
+
+
     }
 }
