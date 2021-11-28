1be3b9118c6bc68e001e73ba109ace68d6e84794
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-11-28 20:12:20 +0600

    Used Item Repo  from Item Controller

--- PlayCatelogServiceApp/Controllers/ItemsController.cs
+++ PlayCatelogServiceApp/Controllers/ItemsController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PlayCatelogServiceApp.Helpers;
+using PlayCatelogServiceApp.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace PlayCatelogServiceApp.Controllers
 {
@@ -10,6 +13,9 @@ namespace PlayCatelogServiceApp.Controllers
     [ApiController]
     public class ItemsController : ControllerBase
     {
+
+        private readonly ItemRepository _itemRepository = new();
+
         private static readonly List<ItemDto> items = new List<ItemDto>()
         {
             new ItemDto(Guid.NewGuid(),"Pen","Red Color Pen",10,DateTimeOffset.UtcNow),
@@ -19,65 +25,88 @@ namespace PlayCatelogServiceApp.Controllers
         };
 
         [HttpGet]
-        public IEnumerable<ItemDto> Get()
+        public async Task<IEnumerable<ItemDto>> Get()
         {
-            return items;
+            var itemsToReturn = (await _itemRepository.GetAllSync()).Select(x => x.AsDto());
+            return itemsToReturn;
         }
 
         [HttpGet("{id}")]
-        public ActionResult<ItemDto> GetItemById(Guid id)
+        public async Task<ActionResult<ItemDto>> GetItemById(Guid id)
         {
-            var item = items.Where(x => x.Id == id).SingleOrDefault();
+            var item = await _itemRepository.GetSync(id);
+            // var item = items.Where(x => x.Id == id).SingleOrDefault();
 
             if (item == null)
             {
                 return NotFound();
             }
-            return item;
+            return item.AsDto();
         }
 
 
         [HttpPost]
-        public ActionResult<ItemDto> Post(CreateItemDto createItemDto)
+        public async Task<ActionResult<ItemDto>> Post(CreateItemDto createItemDto)
         {
-            var item = new ItemDto(Guid.NewGuid(), createItemDto.Name, createItemDto.Description, createItemDto.Price, DateTimeOffset.UtcNow);
-            items.Add(item);
+            //var item = new ItemDto(Guid.NewGuid(), createItemDto.Name, createItemDto.Description, createItemDto.Price, DateTimeOffset.UtcNow);
+            //items.Add(item);
+            var item = createItemDto.AsCreateModel();
+            await _itemRepository.CreateSync(item);
             return CreatedAtAction(nameof(GetItemById), new { id = item.Id }, item);
         }
 
         [HttpPut("{id}")]
-        public ActionResult<ItemDto> Put(Guid id, UpdateItemDto updateItemDto)
+        public async Task<ActionResult<ItemDto>> Put(Guid id, UpdateItemDto updateItemDto)
         {
-            var item = items.Where(x => x.Id == id).SingleOrDefault();
+            //var item = items.Where(x => x.Id == id).SingleOrDefault();
 
-            if (item == null)
+            //if (item == null)
+            //{
+            //    return NotFound();
+            //}
+
+            //var updateItem = item with
+            //{
+            //    Name = updateItemDto.Name,
+            //    Description = updateItemDto.Description,
+            //    Price = updateItemDto.Price,
+            //};
+
+            //var index = items.FindIndex(x => x.Id == id);
+            //items[index] = updateItem;
+
+            var item = updateItemDto.AsUpdateModel(id);
+            var itemFromDB = await _itemRepository.GetSync(id);
+            if (itemFromDB == null)
             {
                 return NotFound();
             }
 
-            var updateItem = item with
-            {
-                Name = updateItemDto.Name,
-                Description = updateItemDto.Description,
-                Price = updateItemDto.Price,
-            };
+            await _itemRepository.UpdateSync(item);
 
-            var index = items.FindIndex(x => x.Id == id);
-            items[index] = updateItem;
             return CreatedAtAction(nameof(GetItemById), new { id = item.Id }, item);
         }
 
 
         [HttpDelete("{id}")]
-        public IActionResult Delete(Guid id)
+        public async Task<IActionResult> Delete(Guid id)
         {
-            var index = items.FindIndex(x => x.Id == id);
+            //var index = items.FindIndex(x => x.Id == id);
 
-            if (index < 0)
+            //if (index < 0)
+            //{
+            //    return NotFound();
+            //}
+            //items.RemoveAt(index);
+
+            var itemFromDB = await _itemRepository.GetSync(id);
+            if (itemFromDB == null)
             {
                 return NotFound();
             }
-            items.RemoveAt(index);
+
+            await _itemRepository.RemoveSync(itemFromDB);
+
             return NoContent();
         }
 
