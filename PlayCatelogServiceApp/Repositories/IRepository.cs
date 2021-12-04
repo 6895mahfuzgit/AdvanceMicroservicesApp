c9264158dda9b32f33a697666ce444502b5621b6
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-12-05 02:24:15 +0600

    Create IRepository.cs

---
+++ PlayCatelogServiceApp/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using PlayCatelogServiceApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PlayCatelogServiceApp.Repositories
+{
+    public interface IRepository<T> where T : IEntity
+    {
+        Task CreateSync(T entity);
+        Task<IReadOnlyCollection<T>> GetAllSync();
+        Task<T> GetSync(Guid id);
+        Task RemoveSync(T entity);
+        Task UpdateSync(T entity);
+    }
+}
