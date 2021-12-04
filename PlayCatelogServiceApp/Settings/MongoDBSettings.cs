11ac12d6037b8a2eeec5b37d3daef909d68bc7e6
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-12-05 00:34:10 +0600

    Create MongoDBSettings.cs

---
+++ PlayCatelogServiceApp/Settings/MongoDBSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace PlayCatelogServiceApp.Settings
+{
+    public class MongoDBSettings
+    {
+        public string Host { get; init; }
+        public string Port { get; init; }
+        public string ConnectionString => $"mongodb://{Host}:{Port}";
+    }
+}
