c4694a78be2283ab2586b4e946e8a3221dadea40
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-12-05 02:23:55 +0600

    Update Item.cs

--- PlayCatelogServiceApp/Entities/Item.cs
+++ PlayCatelogServiceApp/Entities/Item.cs
@@ -2,7 +2,8 @@
 
 namespace PlayCatelogServiceApp.Entities
 {
-    public class Item
+
+    public class Item : IEntity
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
