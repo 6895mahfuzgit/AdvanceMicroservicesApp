3d44e96c8e27d66a869c346d62ec30a187d20cc4
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-12-05 17:17:36 +0600

    Create IRepository.cs

---
+++ PlayCommonApp/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using PlayCommonApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PlayCommonApp.Repositories
+{
+    public interface IRepository<T> where T : IEntity
+    {
+        Task CreateSync(T entity);
+        Task<IReadOnlyCollection<T>> GetAllSync();
+        Task<T> GetSync(Guid id);
+        Task RemoveSync(T entity);
+        Task UpdateSync(T entity);
+    }
+}
