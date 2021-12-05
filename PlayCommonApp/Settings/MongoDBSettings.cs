293ecf2d7d1026997423428ff92a3b28d9dbf5b6
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-12-05 17:11:50 +0600

    Added Settings Models

---
+++ PlayCommonApp/Settings/MongoDBSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace PlayCommonApp.Settings
+{
+    public class MongoDBSettings
+    {
+        public string Host { get; init; }
+        public string Port { get; init; }
+        public string ConnectionString => $"mongodb://{Host}:{Port}";
+    }
+}
---
+++ PlayCommonApp/Settings/ServiceSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace PlayCommonApp.Settings
+{
+    public class ServiceSettings
+    {
+        public string ServiceName { get; init; }
+    }
+}
