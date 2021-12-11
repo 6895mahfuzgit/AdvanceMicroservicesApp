a2ad455e440990eb6cb610dae95ce9987e6ada7e
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-12-11 20:36:09 +0600

    Create Dtos.cs

---
+++ PlayInventoryServiceApp/Dtos.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace PlayInventoryServiceApp
+{
+    public record GrandItemDto(Guid UserId, Guid CatagoryId, int Quantity);
+    public record InventoryItemDto(Guid CatagoryId, int Quantity, DateTimeOffset AcquiredDate)
+}
