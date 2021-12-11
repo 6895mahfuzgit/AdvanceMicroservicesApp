7500c09423d252cb071afbf0a0a1ba16b34faae4
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-12-11 20:36:35 +0600

    Update InventoryItem.cs

--- PlayInventoryServiceApp/Models/InventoryItem.cs
+++ PlayInventoryServiceApp/Models/InventoryItem.cs
@@ -6,12 +6,9 @@ namespace PlayInventoryServiceApp.Models
     public class InventoryItem : IEntity
     {
         public Guid Id { get; set; }
-
         public Guid UserId { get; set; }
         public Guid CatagoryItemId { get; set; }
-
         public int Quantity { get; set; }
-
         public DateTimeOffset AcquiredDate { get; set; }
     }
 }
