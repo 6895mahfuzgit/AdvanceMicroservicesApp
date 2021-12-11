eb9a9b5120444335d8b47d5541c9fea1134acab1
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-12-11 20:36:13 +0600

    Create InventoryItem.cs

---
+++ PlayInventoryServiceApp/Models/InventoryItem.cs
@@ -0,0 +1,17 @@
+﻿using PlayCommonApp.Entities;
+using System;
+
+namespace PlayInventoryServiceApp.Models
+{
+    public class InventoryItem : IEntity
+    {
+        public Guid Id { get; set; }
+
+        public Guid UserId { get; set; }
+        public Guid CatagoryItemId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public DateTimeOffset AcquiredDate { get; set; }
+    }
+}
