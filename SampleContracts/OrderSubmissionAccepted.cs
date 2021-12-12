130474e578527f0f8136569f01de4dab75b796b7
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-12-12 23:46:54 +0600

    Create OrderSubmissionAccepted.cs

---
+++ SampleContracts/OrderSubmissionAccepted.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SampleContracts
+{
+    public interface OrderSubmissionAccepted
+    {
+        Guid OrderId { get; }
+        DateTime Timestamp { get; }
+        string CustomerNumber { get; }
+    }
+}
