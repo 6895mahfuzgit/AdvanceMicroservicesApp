4d37d344aa17f69f496d8f02b48d114c88bf9b51
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-12-16 20:29:42 +0600

    Update Dtos.cs

--- PlayInventoryServiceApp/Dtos.cs
+++ PlayInventoryServiceApp/Dtos.cs
@@ -4,4 +4,7 @@ namespace PlayInventoryServiceApp
 {
     public record GrandItemDto(Guid UserId, Guid CatagoryId, int Quantity);
     public record InventoryItemDto(Guid CatagoryId, int Quantity, DateTimeOffset AcquiredDate);
+
+    public record CatalogItemDto(Guid Id, string Name, string Description);
+
 }
