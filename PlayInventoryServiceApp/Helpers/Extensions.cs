a4c5b46465a764f6ec6a7af27f3c769f0c42d57e
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-12-16 20:57:11 +0600

    Update Extensions.cs

--- PlayInventoryServiceApp/Helpers/Extensions.cs
+++ PlayInventoryServiceApp/Helpers/Extensions.cs
@@ -4,9 +4,9 @@ namespace PlayInventoryServiceApp.Helpers
 {
     public static class Extensions
     {
-        public static InventoryItemDto AsDto(this InventoryItem inventoryItem)
+        public static InventoryItemDto AsDto(this InventoryItem inventoryItem, string Name, string Description)
         {
-            return new InventoryItemDto(inventoryItem.CatagoryItemId, inventoryItem.Quantity, inventoryItem.AcquiredDate);
+            return new InventoryItemDto(inventoryItem.CatagoryItemId, Name, Description, inventoryItem.Quantity, inventoryItem.AcquiredDate);
         }
 
         public static InventoryItem AsModel(this GrandItemDto grandItemDto)
