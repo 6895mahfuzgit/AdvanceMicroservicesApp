f991d9bd6dedab5982d798dc177092327ca8257a
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-12-16 20:57:02 +0600

    Create CatalogClient.cs

---
+++ PlayInventoryServiceApp/Clients/CatalogClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace PlayInventoryServiceApp.Clients
+{
+    public class CatalogClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public CatalogClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+
+        public async Task<IReadOnlyCollection<CatalogItemDto>> GetCatalogAsync()
+        {
+            try
+            {
+                var items = await _httpClient.GetFromJsonAsync<IReadOnlyCollection<CatalogItemDto>>("/items");
+                return items;
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+    }
+}
